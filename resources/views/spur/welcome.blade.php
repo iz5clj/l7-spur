9446d0263e7cf2e5fa6423fbe2098157f356e390
Michel <ms@auxe.net>
2020-05-07 07:22:31 +0000

    First commit

--- resources/views/spur/welcome.blade.php
+++ resources/views/spur/welcome.blade.php
@@ -1,27 +1,27 @@
-<section class="mt-5" id="colors">
+<div id="colors">
     @include('spur.colors')
-</section>
-<section class="mt-5" id="cards">
+</div>
+<section id="cards">
     @include('spur.cards')
 </section>
-<section class="mt-5" id="charts">
+<section id="charts">
     @include('spur.charts')
 </section>
-<section class="mt-5" id="forms">
+<section id="forms">
     @include('spur.forms')
 </section>
-<section class="mt-5" id="stats">
+<section id="stats">
     @include('spur.stats')
 </section>
-<section class="mt-5" id="tables">
+<section id="tables">
     @include('spur.tables')
 </section>
-<section class="mt-5" id="typography">
+<section id="typography">
     @include('spur.typography')
 </section>
-<section class="mt-5" id="user-interface">
+<section id="user-interface">
     @include('spur.user-interface')
 </section>
-<section class="mt-5" id="icons">
+<section id="icons">
     @include('spur.icons')
 </section>
